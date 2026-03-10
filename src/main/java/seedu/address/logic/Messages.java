73a4beb09963dec8c94fa726193b0c4131a22fb0
Gao Huiying <e1481825@u.nus.edu>
2026-03-11 03:53:10 +0800

    Fix Messages.java

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,10 +14,10 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_INVALID_APPLICATION_DISPLAYED_INDEX = "The application index provided is invalid";
+    public static final String MESSAGE_APPLICATIONS_LISTED_OVERVIEW = "%1$d applications listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
-                "Multiple values specified for the following single-valued field(s): ";
+            "Multiple values specified for the following single-valued field(s): ";
 
     /**
      * Returns an error message indicating the duplicate prefixes.
@@ -32,17 +32,17 @@ public class Messages {
     }
 
     /**
-     * Formats the {@code person} for display to the user.
+     * Formats the {@code application} for display to the user.
      */
     public static String format(Application application) {
         final StringBuilder builder = new StringBuilder();
-        builder.append(application.getName())
+        builder.append(application.getRole())
                 .append("; Phone: ")
                 .append(application.getPhone())
                 .append("; Email: ")
-                .append(application.getEmail())
-                .append("; Address: ")
-                .append(application.getAddress())
+                .append(application.getHrEmail())
+                .append("; Company: ")
+                .append(application.getCompany())
                 .append("; Tags: ");
         application.getTags().forEach(builder::append);
         return builder.toString();
