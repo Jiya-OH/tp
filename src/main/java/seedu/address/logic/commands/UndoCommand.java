1d937df94909c522d599fa78c43120790b76e125
Guo Xingchen <e1459295@u.nus.edu>
2026-03-23 20:03:47 +0800

    Add UndoCommand and new AddressBook

---
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPLICATIONS;
+
+public class UndoCommand extends Command {
+    public static final String COMMAND_WORD = "undo";
+
+    public static final String MESSAGE_SUCCESS = "Undo Successfully！";
+    public static final String MESSAGE_FAILURE = "There are no operations that can be undone";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (!model.canUndoAddressBook()) {
+            throw new CommandException(MESSAGE_FAILURE);
+        }
+
+        model.undoAddressBook();
+        model.updateFilteredApplicationList(PREDICATE_SHOW_ALL_APPLICATIONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+}
